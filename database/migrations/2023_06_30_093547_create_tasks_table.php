13bdad21fe2425f87b287f69c4d4c254b567c120
gedons <gedoniani@gmail.com>
2023-06-30 11:01:34 +0100

    migrations, models, and controllers for the users and tasks.

---
+++ database/migrations/2023_06_30_093547_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->date('due_date');
+            $table->enum('priority', ['high', 'medium', 'low'])->default('high');
+            $table->enum('status', ['finished', 'pending'])->default('pending');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
